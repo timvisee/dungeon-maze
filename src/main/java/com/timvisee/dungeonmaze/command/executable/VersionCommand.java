d8272d6320b26e40991ff9a21297efd7d8b4fbf7
Tim Visée <timvisee@gmail.com>
2015-10-01 16:24:54 +0200

    Updated license file reference in version command

--- src/main/java/com/timvisee/dungeonmaze/command/executable/VersionCommand.java
+++ src/main/java/com/timvisee/dungeonmaze/command/executable/VersionCommand.java
@@ -30,7 +30,7 @@ public class VersionCommand extends ExecutableCommand {
         printDeveloper(sender, "sgdc3", "sgdc3", "Code Contributor");
         printDeveloper(sender, "Metonymia", "Metonymia", "Design Contributor");
         sender.sendMessage(ChatColor.GOLD + "Website: " + ChatColor.WHITE + "http://timvisee.com/projects/bukkit/dungeon-maze/");
-        sender.sendMessage(ChatColor.GOLD + "License: " + ChatColor.WHITE + "GNU GPL v3.0" + ChatColor.GRAY + ChatColor.ITALIC + " (See LICENSE.txt)");
+        sender.sendMessage(ChatColor.GOLD + "License: " + ChatColor.WHITE + "GNU GPL v3.0" + ChatColor.GRAY + ChatColor.ITALIC + " (See LICENSE file)");
         sender.sendMessage(ChatColor.GOLD + "Copyright: " + ChatColor.WHITE + "Copyright (c) Tim Visee 2015. All rights reserved.");
         return true;
     }
