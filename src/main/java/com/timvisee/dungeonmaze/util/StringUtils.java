3dd7a07fe858020708caa3bc8b70ace20bfac775
Tim Visée <timvisee@gmail.com>
2015-11-22 21:05:01 +0100

    Created method to get a strack trace as a string

--- src/main/java/com/timvisee/dungeonmaze/util/StringUtils.java
+++ src/main/java/com/timvisee/dungeonmaze/util/StringUtils.java
@@ -4,6 +4,9 @@ import net.ricecode.similarity.LevenshteinDistanceStrategy;
 import net.ricecode.similarity.StringSimilarityService;
 import net.ricecode.similarity.StringSimilarityServiceImpl;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class StringUtils {
 
     /**
@@ -23,5 +26,24 @@ public class StringUtils {
         double score = service.score(first, second);
 
         return Math.abs(score - 1.0);
+    }
+
+    /**
+     * Get a full stack trace of an exception as a string.
+     *
+     * @param exception The exception.
+     *
+     * @return Stack trace as a string.
+     */
+    public static String getStackTrace(Exception exception) {
+        // Create a string and print writer to print the stack trace into
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        // Print the stack trace into the print writer
+        exception.printStackTrace(printWriter);
+
+        // Return the result as a string
+        return stringWriter.toString();
     }
 }
