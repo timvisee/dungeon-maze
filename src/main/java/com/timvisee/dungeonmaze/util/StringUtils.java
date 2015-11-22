eedb617e7ffffe34d3311379807baf62c7ce276e
Tim Visée <timvisee@gmail.com>
2015-11-22 21:04:46 +0100

    Added basic JavaDocs for getDifference method in StringUtils

--- src/main/java/com/timvisee/dungeonmaze/util/StringUtils.java
+++ src/main/java/com/timvisee/dungeonmaze/util/StringUtils.java
@@ -6,6 +6,14 @@ import net.ricecode.similarity.StringSimilarityServiceImpl;
 
 public class StringUtils {
 
+    /**
+     * Get the difference value of a string.
+     *
+     * @param first First value.
+     * @param second Second value.
+     *
+     * @return The string difference value.
+     */
     public static double getDifference(String first, String second) {
         if(first == null || second == null)
             return 1.0;
