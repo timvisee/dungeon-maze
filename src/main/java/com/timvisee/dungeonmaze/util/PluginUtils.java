36d564c15ae97f4323076e2690c778f376641e00
Tim Visée <timvisee@gmail.com>
2015-11-26 22:04:32 +0100

    Created getPluginFile method in plugin utilities class

--- src/main/java/com/timvisee/dungeonmaze/util/PluginUtils.java
+++ src/main/java/com/timvisee/dungeonmaze/util/PluginUtils.java
@@ -1,7 +1,9 @@
 package com.timvisee.dungeonmaze.util;
 
+import com.timvisee.dungeonmaze.DungeonMaze;
 import org.bukkit.Bukkit;
 
+import java.io.File;
 import java.net.URL;
 
 public class PluginUtils {
@@ -9,9 +11,19 @@ public class PluginUtils {
     /**
      * Get the URL to the JAR file of the plugin.
      *
-     * @return URL.
+     * @return Plugin file URL.
      */
     public static URL getPluginFileUrl() {
         return Bukkit.getPluginManager().getPlugin("PluginName").getClass().getProtectionDomain().getCodeSource().getLocation();
+    }
+
+    /**
+     * Get the JAR file of the plugin.
+     * Note: The use of the getPluginFileUrl method is recommended if possible.
+     *
+     * @return Plugin file.
+     */
+    public static File getPluginFile() {
+        return new File(DungeonMaze.instance.getDataFolder().getParentFile(), "DungeonMaze.jar");
     }
 }
