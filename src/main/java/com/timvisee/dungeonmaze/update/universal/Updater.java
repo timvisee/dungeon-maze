2d9b8e9cc611b6d4318fe4b838daa56713ac2404
timvisee <timvisee@gmail.com>
2015-12-01 11:15:44 +0100

    Created basis of universal updater

--- src/main/java/com/timvisee/dungeonmaze/update/universal/Updater.java
+++ src/main/java/com/timvisee/dungeonmaze/update/universal/Updater.java
@@ -1,3 +1,74 @@
 package com.timvisee.dungeonmaze.update.universal;
 
-public class Updater { }
+import com.timvisee.dungeonmaze.DungeonMaze;
+
+public class Updater {
+
+    /**
+     * True to automatically download the update, if there's one available that is compatible.
+     */
+    private boolean autoDownload;
+    /**
+     * True to automatically install the upate, if there's one available that is compatible.
+     */
+    private boolean autoInstall;
+
+    /**
+     * Constructor.
+     *
+     * @param check True to do an update check.
+     * @param autoDownload True to automatically download a new and compatible update.
+     * @param autoInstall True to automatically install a new and compatible update.
+     */
+    public Updater(boolean check, boolean autoDownload, boolean autoInstall) {
+        // Store the automatically download and install configuration
+        this.autoDownload = autoDownload;
+        this.autoInstall = autoInstall;
+
+        // Should we do an update check
+        if(check)
+            check();
+    }
+
+    /**
+     * Check if there's an new update available.
+     * Also verify whether this update is compatible with the current installation or not.
+     *
+     * @return True if succeed, false if failed.
+     */
+    public boolean checkUpdates() {
+        // TODO: Do a version check
+    }
+
+    /**
+     * Download the newest update. The newest update is only downloaded if it's newer than the current installed version,
+     * and if it's compatible with the current Bukkit and Java installation.
+     * Note: False will also be returned if the update wasn't downloaded because it's not newer than the currently
+     * installed version, or if it's not compatible with the current server set up.
+     *
+     * @return True of succeed, false on failure.
+     */
+    public boolean downloadUpdate() {
+        // TODO: Make sure we recently checked for an update!
+        // TODO: Make sure the file is compatible?
+        // TODO: Download the file.
+    }
+
+    /**
+     * Download and installUpdate the newest version available if compatible.
+     *
+     * @return True if succeed, false on failure.
+     */
+    public boolean installUpdate() {
+        // TODO: Make sure the file is downloaded, and can be downloaded.
+    }
+
+    /**
+     * Get the Dungeon Maze instance.
+     *
+     * @return Dungeon Maze.
+     */
+    public DungeonMaze getDungeonMaze() {
+        return DungeonMaze.instance;
+    }
+}
