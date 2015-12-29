a24120e17148b7902d031cdf9fe63b44e261adbf
Tim Visée <timvisee@gmail.com>
2015-12-29 20:20:32 +0100

    Removed unused imports

--- src/main/java/com/timvisee/dungeonmaze/update/universal/UniversalUpdater.java
+++ src/main/java/com/timvisee/dungeonmaze/update/universal/UniversalUpdater.java
@@ -4,16 +4,12 @@ import com.timvisee.dungeonmaze.DungeonMaze;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.Iterator;
 
 public class UniversalUpdater {
 
