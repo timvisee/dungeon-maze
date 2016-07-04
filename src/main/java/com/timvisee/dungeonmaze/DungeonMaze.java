7ea4ac50521c3bb7caa7f17e0e0351c186df4f47
Tim Visée <timvisee@gmail.com>
2016-07-04 14:03:30 +0200

    Remove incomplete scheduled populator test from master branch

--- src/main/java/com/timvisee/dungeonmaze/DungeonMaze.java
+++ src/main/java/com/timvisee/dungeonmaze/DungeonMaze.java
@@ -79,28 +79,6 @@ public class DungeonMaze extends JavaPlugin {
         // Show a startup message
         Core.getLogger().info(getVersionComplete(true) + " started, took " + profiler.getTimeFormatted() + "!");
         Core.getLogger().info(getPluginName() + " developed by Tim Visee - timvisee.com");
-
-        // TODO: Test
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            if(queuedPopulators.size() > 0) {
-                for(int i = 0; i < 20; i++) {
-                    // Get the queued populator
-                    DelayedPopulator tc = queuedPopulators.get(0);
-
-                    if(queuedPopulators.size() < 10 || Math.random() < 0.0005 / 2.0)
-                        Core.getLogger().debug("Populating at (" + tc.getChunk().getX() + ", " + tc.getChunk().getZ() + "), " + queuedPopulators.size() + " left...");
-
-                    tc.populate();
-
-                    // Remove the test class
-                    queuedPopulators.remove(0);
-                }
-
-                if(queuedPopulators.size() == 0) {
-                    Core.getLogger().debug("Finished populating chunks!");
-                }
-            }
-        }, 1, 1);
     }
 
     /**
